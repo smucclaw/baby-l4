1949516b60b828bf4440fc2f67d19a4752ca8fa8
Andreas Källberg <anka.213@gmail.com>
2021-02-19 00:07:34 +0800

    Automatically build gf files with cabal
    Note that this is a bit buggy and might not rebuild it on every change

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,39 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.PreProcess
+import Distribution.PackageDescription
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Program
+import System.FilePath ((</>))
+
+main :: IO ()
+main = defaultMainWithHooks userHooks
+
+userHooks :: UserHooks
+userHooks = simpleUserHooks {
+    hookedPreProcessors = gfPPSuffix : knownSuffixHandlers
+}
+
+gfPPSuffix :: PPSuffixHandler
+gfPPSuffix = ("gf" , gfPP)
+
+gfPP :: BuildInfo -> LocalBuildInfo -> ComponentLocalBuildInfo -> PreProcessor
+gfPP bi lbi clbi = PreProcessor {
+    platformIndependent = True,
+    runPreProcessor = \(inDir,inFile) (outDir,outFile) verbosity -> do
+        putStrLn $ "hello world! " ++ show ((inDir,inFile), (outDir,outFile), verbosity)
+        let args =
+                [ "-make"
+                , "-f", "haskell"
+                , "-output-dir=" ++ outDir
+                , inDir </> inFile
+                ]
+        print args
+        (gfProg, _) <- requireProgram verbosity gfProgram (withPrograms lbi)
+        -- runDbProgram verbosity gfProgram (withPrograms lbi) args
+        runProgram verbosity gfProg args
+        putStrLn "Done running"
+}
+
+gfProgram :: Program
+gfProgram = simpleProgram "gf"
+
