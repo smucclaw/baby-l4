161085f38d89b1ab9b1c6f67242ed9aafbd07cc5
Andreas Källberg <anka.213@gmail.com>
2021-03-03 17:44:20 +0800

    Add source locations to the AST and prepare for LSP use
    Co-authored-by: Jacob Tan <2jacobtan@users.noreply.github.com>

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,10 +1,15 @@
 
 {-# LANGUAGE DeriveTraversable #-}
+-- {-# LANGUAGE NoFieldSelectors #-}
+{-# OPTIONS_GHC -Wpartial-fields #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 module Syntax where
 
+
 -- Class for annotated expressions
-class HasAnn f where
-  getAnn :: f a -> a
+import qualified Language.LSP.Types as J
+import qualified Data.List as List
+import Data.Data (Data, Typeable)
 
 
 ----------------------------------------------------------------------
@@ -15,19 +20,40 @@ class HasAnn f where
 ----- Names
 type VarName = String
 type RuleName = String
+-- newtype VarName = VarName String
+--   deriving (Eq, Ord, Show, Read, Data, Typeable)
+-- newtype RuleName = RuleName String
+--   deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 newtype ClassName = ClsNm String
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 newtype FieldName = FldNm String
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 newtype PartyName = PtNm String
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
+class HasLoc a where
+  getLoc :: a -> SRng
+
+
+data Pos = Pos
+  { line :: !Int
+  , col  :: !Int
+  }
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
+data SRng = SRng
+  { start :: Pos
+  , end   :: Pos
+  }
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
+
+instance HasLoc SRng where
+  getLoc = id
 
 ----- Program
 
 data Program ct et = Program [Mapping] [ClassDecl ct] [VarDecl] [Rule et] [Assertion et]
-  deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
+  deriving (Eq, Ord, Show, Read, Functor, Data, Typeable)
 
 removeAnnotations :: Program ct et -> Program ct ()
 removeAnnotations = (()<$)
@@ -40,25 +66,25 @@ data Tp
   | FunT Tp Tp
   | TupleT [Tp]
   | ErrT
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 data VarDecl = VarDecl VarName Tp
-  deriving (Eq, Ord, Show, Read)
-data Mapping = Mapping VarName VarName
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
+data Mapping = Mapping SRng VarName VarName
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- Field attributes: for example cardinality restrictions
 -- data FieldAttribs = FldAtt
 data FieldDecl = FieldDecl FieldName Tp -- FieldAttribs
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- superclass, list of field declarations
 data ClassDef t = ClassDef t [FieldDecl]
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Functor, Data, Typeable)
 
 -- declares class with ClassName and definition as of ClassDef
 data ClassDecl t = ClassDecl ClassName (ClassDef t)
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Functor, Data, Typeable)
 
 name_of_class_decl :: ClassDecl t -> ClassName
 name_of_class_decl (ClassDecl cn _) = cn
@@ -112,92 +138,125 @@ data Val
     -- TODO: instead of RecordV, introduce RecordE in type Expr
     | RecordV ClassName [(FieldName, Val)]
     | ErrV
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 data Var
     = GlobalVar VarName             -- global variable only known by its name
     | LocalVar VarName Int          -- local variable known by its provisional name and deBruijn index.
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- unary arithmetic operators
 data UArithOp = UAminus
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- unary boolean operators
 data UBoolOp = UBneg
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- unary operators (union of the above)
 data UnaOp
     = UArith UArithOp
     | UBool UBoolOp
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- binary arithmetic operators
 data BArithOp = BAadd | BAsub | BAmul | BAdiv | BAmod
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- binary comparison operators
 data BComparOp = BCeq | BClt | BClte | BCgt | BCgte | BCne
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- binary boolean operators
 data BBoolOp = BBimpl | BBor | BBand
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- binary operators (union of the above)
 data BinOp
     = BArith BArithOp
     | BCompar BComparOp
     | BBool BBoolOp
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- operators for combining list elements
 data ListOp = AndList | OrList | XorList | CommaList
-    deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 data Pattern
     = VarP String
     | VarListP [String]
-    deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 data Quantif = All | Ex
-    deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read, Data, Typeable)
+
+
+data FooBar
+  = Foo {x :: Int, _y :: Char}
+  | Bar {x :: Int}
 
 
 -- Expr t is an expression of type t (to be determined during type checking / inference)
 data Expr t
-    = ValE t Val                                -- value
-    | VarE t Var                                -- variable
-    | UnaOpE t UnaOp (Expr t)                   -- unary operator
-    | BinOpE t BinOp (Expr t) (Expr t)          -- binary operator
-    | IfThenElseE t (Expr t) (Expr t) (Expr t)  -- conditional
-    | AppE t (Expr t) (Expr t)                  -- function application
-    | FunE t Pattern Tp (Expr t)                -- function abstraction
-    | QuantifE t Quantif VarName Tp (Expr t)    -- quantifier
-    | ClosE t [(VarName, Expr t)] (Expr t)      -- closure  (not externally visible)
-    | FldAccE t (Expr t) FieldName              -- field access
-    | TupleE t [Expr t]                         -- tuples
-    | CastE t Tp (Expr t)                       -- cast to type
-    | ListE t ListOp [Expr t]                   -- list expression
-    deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
-instance HasAnn Expr where
-  getAnn = tpOfExpr
+    = ValE        SRng t Val                          -- value
+    | VarE        SRng t Var                          -- variable
+    | UnaOpE      SRng t UnaOp (Expr t)               -- unary operator
+    | BinOpE      SRng t BinOp (Expr t) (Expr t)      -- binary operator
+    | IfThenElseE SRng t (Expr t) (Expr t) (Expr t)   -- conditional
+    | AppE        SRng t (Expr t) (Expr t)            -- function application
+    | FunE        SRng t Pattern Tp (Expr t)          -- function abstraction
+    | QuantifE    SRng t Quantif VarName Tp (Expr t)  -- quantifier
+    | ClosE       SRng t [(VarName, Expr t)] (Expr t) -- closure  (not externally visible)
+    | FldAccE     SRng t (Expr t) FieldName           -- field access
+    | TupleE      SRng t [Expr t]                     -- tuples
+    | CastE       SRng t Tp (Expr t)                  -- cast to type
+    | ListE       SRng t ListOp [Expr t]              -- list expression
+    deriving (Eq, Ord, Show, Read, Functor, Data, Typeable)
+instance HasLoc (Expr t) where
+  getLoc x = case x of
+    ValE        loc _ _       -> loc
+    VarE        loc _ _       -> loc
+    UnaOpE      loc _ _ _     -> loc
+    BinOpE      loc _ _ _ _   -> loc
+    IfThenElseE loc _ _ _ _   -> loc
+    AppE        loc _ _ _     -> loc
+    FunE        loc _ _ _ _   -> loc
+    QuantifE    loc _ _ _ _ _ -> loc
+    FldAccE     loc _ _ _     -> loc
+    TupleE      loc _ _       -> loc
+    CastE       loc _ _ _     -> loc
+    ListE       loc _ _ _     -> loc
+
+childExprs :: Expr t -> [Expr t]
+childExprs x = case x of
+    ValE        _ _ _       -> []
+    VarE        _ _ _       -> []
+    UnaOpE      _ _ _ a     -> [a]
+    BinOpE      _ _ _ a b   -> [a,b]
+    IfThenElseE _ _ i t e   -> [i,t,e]
+    AppE        _ _ f x     -> [f,x]
+    FunE        _ _ _ _ x   -> [x]
+    QuantifE    _ _ _ _ _ x -> [x]
+    FldAccE     _ _ x _     -> [x]
+    TupleE      _ _ xs      -> xs
+    CastE       _ _ _ x     -> [x]
+    ListE       _ _ _ xs     -> xs
+
 
 tpOfExpr :: Expr t -> t
 tpOfExpr x = case x of
-  ValE t _      -> t
-  VarE t _      -> t
-  UnaOpE t _ _  -> t
-  BinOpE t _ _ _  -> t
-  IfThenElseE t _ _ _ -> t
-  AppE t _ _  -> t
-  FunE t _ _ _  -> t
-  QuantifE t _ _ _ _ -> t
-  FldAccE t _ _ -> t
-  TupleE t _ -> t
-  CastE t _ _     -> t
-  ListE t _ _     -> t
+  ValE        _ t _       -> t
+  VarE        _ t _       -> t
+  UnaOpE      _ t _ _     -> t
+  BinOpE      _ t _ _ _   -> t
+  IfThenElseE _ t _ _ _   -> t
+  AppE        _ t _ _     -> t
+  FunE        _ t _ _ _   -> t
+  QuantifE    _ t _ _ _ _ -> t
+  FldAccE     _ t _ _     -> t
+  TupleE      _ t _       -> t
+  CastE       _ t _ _     -> t
+  ListE       _ t _ _     -> t
 
 
 -- Cmd t is a command of type t
@@ -205,39 +264,39 @@ data Cmd t
     = Skip                                      -- Do nothing
     | VAssign Var (Expr t)                   -- Assignment to variable
     | FAssign (Expr t) FieldName (Expr t)         -- Assignment to field
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 
 data Rule t = Rule RuleName [VarDecl] (Expr t) (Expr t)
-  deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
+  deriving (Eq, Ord, Show, Read, Functor, Data, Typeable)
 
 data Assertion t = Assertion (Expr t)
-  deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
+  deriving (Eq, Ord, Show, Read, Functor, Data, Typeable)
 
 ----------------------------------------------------------------------
 -- Definition of Timed Automata
 ----------------------------------------------------------------------
 
 data Clock = Cl String
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- Clock constraint of the form x < c.
 -- TODO: Reconsider the Integer. Might also be a subclass of the "Time" class
 data ClConstr = ClCn Clock BComparOp Integer
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 data Loc = Lc String
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- Synchronization type: send or receive
 data Sync = Snd | Rec
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- Action in a transition: the string is the ClassName of a subclass of Event
 data Action
   = Internal
   | Act ClassName Sync
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 action_name :: Action -> [ClassName]
 action_name Internal = []
@@ -245,11 +304,11 @@ action_name (Act cn s) = [cn]
 
 -- Transition condition: clock constraints and Boolean expression
 data TransitionCond t = TransCond [ClConstr] (Expr t)
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- Transition action: synchronization action; clock resets; and execution of command (typically assignments)
 data TransitionAction t = TransAction Action [Clock] (Cmd t)
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 transition_action_name :: TransitionAction t -> [ClassName]
 transition_action_name (TransAction act _ _) = action_name act
@@ -257,7 +316,7 @@ transition_action_name (TransAction act _ _) = action_name act
 -- Transition relation from location to location via Action,
 -- provided [ClConstr] are satisfied; and resetting [Clock]
 data Transition t = Trans Loc (TransitionCond t) (TransitionAction t) Loc
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- Timed Automaton having:
 -- a name
@@ -273,12 +332,12 @@ data Transition t = Trans Loc (TransitionCond t) (TransitionAction t) Loc
 -- Note: the set of locations, actions, clocks could in principle be inferred from the remaining info.
 -- Type parameter t: type of expressions: () or Tp, see function Typing/tpExprr
 data TA t = TmdAut String [Loc] [ClassName] [Clock] [Transition t] [Loc] [(Loc, [ClConstr])] [(Loc, Expr t)]
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- Timed Automata System: a set of TAs running in parallel
 -- Type parameter ext: Environment-specific extension
 data TASys t ext = TmdAutSys [TA t] ext
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 name_of_ta :: TA t -> String
 name_of_ta (TmdAut nm ta_locs ta_act_clss ta_clks trans init_locs invs lbls) = nm
@@ -298,11 +357,11 @@ channels_of_ta (TmdAut nm ta_locs ta_act_clss ta_clks trans init_locs invs lbls)
 data Event t
   = EventClConstr ClConstr
   | EventCond (Expr t)
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- only Must and May, assuming that Shant can be compiled away during syntax analysis
 data Modality = Must | May
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
 
 -- EventRule with components:
 -- rule name
@@ -316,4 +375,4 @@ data Modality = Must | May
 -- rule name (optional) corresponding to LEST clause
 
 data EventRule t = EvRule RuleName [Event t] Modality [PartyName] Action [ClConstr] RuleName (Maybe RuleName)
-  deriving (Eq, Ord, Show, Read)
+  deriving (Eq, Ord, Show, Read, Data, Typeable)
