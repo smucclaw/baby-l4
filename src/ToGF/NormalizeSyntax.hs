a1b1534be062eaf016db27d64ec5dd18b5a4c16c
Inari Listenmaa <inari.listenmaa@gmail.com>
2021-04-06 17:15:27 +0800

    Add universal quantification in sCASP translation

--- src/ToGF/NormalizeSyntax.hs
+++ src/ToGF/NormalizeSyntax.hs
@@ -1,16 +1,51 @@
 module ToGF.NormalizeSyntax where
 
+import Annotation (TypeAnnot (updType))
+import Data.Char (toLower)
+import Data.List (intercalate)
+import Data.Maybe (fromMaybe)
 import Syntax
 
-
-normalizeQuantif :: Rule t -> Rule t
-normalizeQuantif (Rule ann nm decls ifE thenE) = 
-  Rule ann nm (decls ++ newDecls) newIfE thenE
+normalizeQuantif :: Rule Tp -> [Rule Tp]
+normalizeQuantif (Rule ann nm decls ifE thenE) =
+  Rule ann nm (decls ++ newDecls) actuallyNewIfE thenE : faRules
   where
-    (newDecls,newIfE) = go ifE -- result of the recursion
-    go (QuantifE ann Ex varnm tp expr) = (VarDecl ann varnm tp:newDs, newE)
-       where (newDs, newE) = go expr
+    (faRules, negApp) = forallRule ifE
+    -- If it was universally quantified, we made a new predicate and now we negate it
+    actuallyNewIfE = fromMaybe newIfE negApp
+    (newDecls, newIfE) = go ifE -- result of the recursion
+    go (QuantifE ann Ex varnm tp expr) = (VarDecl ann varnm tp : newDs, newE)
+      where
+        (newDs, newE) = go expr
     go e = ([], e)
+    forallRule :: Expr Tp -> ([Rule Tp], Maybe (Expr Tp))
+    forallRule (QuantifE ann All name tp ifExp) = ([Rule ann "foo" vardecls ifExp thenExp], Just negThenExp)
+      where
+        vardecls = [VarDecl ann name tp]
+        predName = extractName ifExp
+        newPred = VarE ann (LocalVar predName 1) -- TODO: make it actually unique
+        newArg = VarE tp (LocalVar name 0)
+        thenExp = AppE ann newPred newArg
+        negThenExp = negateExpr thenExp
+    forallRule _ = ([], Nothing)
+
+negateExpr :: Expr t -> Expr t
+negateExpr e = UnaOpE (annotOfExpr e) (UBool UBneg) e
+
+extractName :: Expr t -> String
+extractName (ValE t v) = "someVal"
+extractName (VarE t v) = map toLower $ varName v
+extractName (UnaOpE t u et) = show u ++ extractName et
+extractName (BinOpE t b et et4) = extractName et ++ "_" ++ extractName et4
+extractName (IfThenElseE t et et3 et4) = extractName et ++ "_" ++ extractName et3 ++ "_" ++ extractName et4
+extractName (AppE t et et3) = extractName et ++ "_" ++ extractName et3
+extractName (FunE t p t3 et) = extractName et
+extractName (QuantifE t q l_c t4 et) = extractName et
+extractName (FldAccE t et f) = extractName et
+extractName (TupleE t l_et) = intercalate "_" (map extractName l_et)
+extractName (CastE t t2 et) = extractName et
+extractName (ListE t l l_et) = intercalate "_" (map extractName l_et)
+extractName (NotDeriv t b v et) = extractName et
 
 varName :: Var -> VarName
 varName (GlobalVar n) = n
@@ -22,7 +57,6 @@ normalizeAnd e@(BinOpE ann (BBool BBand) e1 e2) = ListE ann AndList (go e)
     go (BinOpE _ (BBool BBand) e1 e2) = go e1 ++ go e2
     go e = [e]
 normalizeAnd e = e
-
 {- TODO:
 * A function Rule t -> [Rule t], which work on rules like
   if (Rich foo || Pretty foo || Smart foo)
--- src/ToSCASP.hs
+++ src/ToSCASP.hs
@@ -15,23 +15,30 @@ import ToGF.FromL4.ToProp
 
 data Stash ann = Stash
   { rules :: Doc ann,
-    facts :: Doc ann
+    facts :: Doc ann,
+    queries :: Doc ann
   }
 
-rule :: Doc ann -> Stash ann
-rule rl = Stash rl mempty
+unstash :: Stash ann -> Doc ann
+unstash (Stash r f q) = 
+  pretty "% facts" <> f <> line <>
+  pretty "% rules" <> r <> line <>
+  pretty "% queries" <> q
 
-fact :: Doc ann -> Stash ann
-fact fc = Stash mempty fc
+rule, fact, query :: Doc ann -> Stash ann
+rule rl = Stash rl mempty mempty
+fact fc = Stash mempty fc mempty
+query q = Stash mempty mempty q
 
 instance Semigroup (Stash ann) where
-  Stash rules1 facts1 <> Stash rules2 facts2 =
+  Stash rules1 facts1 queries1 <> Stash rules2 facts2 queries2 =
     Stash
       (rules1 <> line <> rules2)
       (facts1 <> line <> facts2)
+      (queries1 <> line <> queries2)
 
 instance Monoid (Stash ann) where
-  mempty = Stash mempty mempty
+  mempty = Stash mempty mempty mempty
 
 isPred :: VarDecl t -> Bool
 isPred = isPred' . tpOfVarDecl
@@ -42,41 +49,25 @@ isPred' (FunT t t2) = isPred' t2
 isPred' _ = False
 
 createSCasp :: Program Tp -> IO ()
-createSCasp p = putDoc (rules $ showSC p <> rule PP.line)
+createSCasp = putDoc . unstash . showSC 
 
 indent' :: Doc ann -> Doc ann
 indent' = indent 4
 
 vsep' :: [Stash ann] -> Stash ann
-vsep' ss = Stash (vsep $ map rules ss) (vsep $ map facts ss)
-
-punctuateBoth :: Doc ann -> [Stash ann] -> Stash ann
-punctuateBoth comma ss = Stash (vsep $ punctuate comma $ map rules ss) (vsep $ punctuate comma $ map facts ss)
-
-punctuateFacts, punctuateRules :: Doc ann -> [Stash ann] -> Stash ann
-punctuateFacts comma ss = Stash (vsep $ map rules ss) (vsep $ punctuate comma $ map facts ss)
-punctuateRules comma ss = Stash (vsep $ punctuate comma $ map rules ss) (vsep $ map facts ss)
-
+vsep' ss = Stash (vsep $ map rules ss) (vsep $ map facts ss) (vsep $ map queries ss)
 class Arg x where
   mkAtom, mkVar :: x -> Doc ann
 
 class SCasp x where
   showSC :: x -> Stash ann
-  showSC = rule . showRule
-  showRule, showFact :: x -> Doc ann
+  showSC = rule . showSingle
+  showSingle :: x -> Doc ann
   showSClist :: [x] -> Stash ann
   showSClist = vsep' . map showSC
-  showRulelist :: [x] -> Doc ann
-  showRulelist = vsep . map showRule
 
-showSCcommalist :: (SCasp a) => [a] -> Doc ann
-showSCcommalist = commaList . map showRule
-
-showSCdotlist :: (SCasp a) => [a] -> Doc ann
-showSCdotlist = dotList . map showRule
-
-onlyPred :: Program t -> [VarDecl t]
-onlyPred = filter isPred . globalsOfProgram
+removePred :: Program t -> [VarDecl t]
+removePred = filter (not . isPred) . globalsOfProgram
 
 commaList :: [Doc ann] -> Doc ann
 commaList = vsep . punctuate comma -- separator: a, b, c
@@ -89,95 +80,68 @@ dotList xs = endDot $ vsep $ punctuate dot xs -- terminator: a. b. c.
 endDot :: Doc ann -> Doc ann
 endDot x = x <> dot
 
--- forallRule :: Expr t -> (Rule t, Doc ann)
--- forallRule (QuantifE ann All name tp ifExp) = (Rule ann "foo" vardecls ifExp thenExp, showRule thenExp)
---   where
---     vardecls = [VarDecl ann name tp]
---     newPred = VarE ann (LocalVar "SuperRandom" 0)
---     newArg = VarE ann (LocalVar name 1)
---     thenExp = AppE ann newPred newArg
--- forallRule _ = error "Trying to apply forall rule to a new expression"
-
 instance SCasp (Program Tp) where
   showSC p =
-    showSClist (assertionsOfProgram p)
-      <> showSClist (onlyPred p)
-      <> showSClist (normalizeQuantif <$> rulesOfProgram p)
-
--- Stash {
--- facts = vsep
---   [ pretty "\n% Facts",
---     showSClist $ assertionsOfProgram p,
---     showSClist $ onlyPred p ],
--- rules = [
---     pretty "\n% Rules",
---     showSClist $ map normalizeQuantif $ rulesOfProgram p
---   ]
-normalizeRule :: a -> [a]
-normalizeRule = (: [])
-
+    showSClist (assertionsOfProgram p) -- TODO: should become queries!!!
+      <> showSClist (removePred p)     -- These become facts
+      <> showSClist (concatMap normalizeQuantif $ rulesOfProgram p) -- These become rules and facts
+  showSingle = unstash . showSC
 instance SCasp (Rule Tp) where
-  showRule (Rule _ rulename vardecls ifExp thenExp) =
+  showSC (Rule _ _ _ (ValE _ (BoolV True)) thenExp) = fact $ endDot $ showSingle thenExp
+  showSC (Rule _ _ _ (ValE _ (BoolV False)) thenExp) = fact $ endDot $ showSingle (negateExpr thenExp)
+  showSC r = rule $ showSingle r
+  showSingle (Rule _ rulename vardecls ifExp thenExp) =
       vsep
-        [ showRule thenExp <+> pretty ":-",
-          endDot $ indent' $ commaList (map showRule vardecls ++ map showRule (normalizeRule ifExp)),
+        [ showSingle thenExp <+> pretty ":-",
+          endDot $ indent' $ commaList (map showSingle vardecls ++ [showSingle ifExp]),
           PP.line
         ]
 
--- We don't want to state as a fact anything that is quantified:
---   `assert exists foo. Legal foo`
--- doesn't mean the same as s(CASP) expression `legal(foo)`.
 -- Only assertions like `assert Beats Rock Scissors` become facts.
+-- Other assertions become rules.
 instance SCasp (Assertion Tp) where
-  showSC (Assertion _ assertExpr) = case assertExpr of
-    QuantifE {} -> mempty
-    _ -> mempty
-    -- _ -> endDot $ showSC assertExpr
-
---showSC (Assertion _ assertExpr) = endDot $ showSC assertExpr
+  showSingle (Assertion foo bar) = line <> pretty "TODO: these become queries" <> line
+  showSC = query . showSingle
 
 instance SCasp (VarDecl t) where
-  showRule (VarDecl _ v tp) = mkAtom tp <> parens (mkVar (v, tp))
-  showRulelist = dotList . map (\(VarDecl _ v tp) -> mkAtom tp <> parens (mkAtom v))
+  -- assume this is used only from Expr! Becomes a part of a Rule.
+  showSingle (VarDecl _ v tp) = mkAtom tp <> parens (mkVar (v, tp))
+
+  -- assume this is used only from Program! Becomes a fact.
+  showSClist = fact . dotList . map decl2fact
+    where decl2fact (VarDecl _ v tp) = mkAtom tp <> parens (mkAtom v)
 
 instance SCasp (Expr Tp) where
-  showRule (Exist x typ exp) = vsep $ existX : suchThat
+  -- We don't need a case for Forall, because normalizeQuantif has taken care of it already earlier
+  showSingle (Exist x typ exp) = vsep $ existX : suchThat
     where
       existX = mkAtom typ <> parens (mkVar (x, typ))
       suchThat =
-        showRule <$> case normalizeAnd exp of
+        showSingle <$> case normalizeAnd exp of
           ListE _ _ es -> es
           _ -> [exp]
-  -- showRule e@(Forall x typ exp) = vsep []
-  --   where
-  --     (newRule, newPred) = forallRule e
-  showRule x = case normalizeAnd x of
-    ValE _ v -> showRule v
+  showSingle x = case normalizeAnd x of
+    ValE _ v -> showSingle v
     FunApp1 f x xTp -> mkAtom f <> parens (mkVar (x, xTp))
     FunApp2 f x xTp y yTp -> mkAtom f <> encloseSep lparen rparen comma (mkVar <$> [(x, xTp), (y, yTp)])
-    ListE _ _ es -> showSCcommalist es
-    UnaOpE _ _ es -> showRule es
-    QuantifE _ _ _ _ es -> showRule es
-    Not x -> showRule x
-    NotDeriv _ boo _ es -> if True then showRulelist [es] else showRule es
+    ListE _ _ es -> commaList $ map showSingle es
+    QuantifE _ _ _ _ es -> showSingle es
+    UnaOpE _ unaop exp -> showSingle unaop <+> showSingle exp
+    NotDeriv ann _ _ e  -> showSingle $ UnaOpE ann (UBool UBneg) e
     --IfThenElseE _ ifE thenE elseE -> vsep [
     --                                  showSC ifE <> comma,
     --                                showSC thenE,
     --                              showSC elseE ]
-    x -> error $ "not handled yet: " ++ show x -- if there's a QuantifE, move it into VarDecls
-
+    x -> error $ "not handled yet: " ++ show x
 --   showSC (BinOpE s t b et et5) = _
 --   showSC (VarE s t v) = _
---   showSC (UnaOpE s t u et) = _
---   showSC (IfThenElseE s t et et4 et5) = _
---   showSC (AppE s t et et4) = _
 --   showSC (FunE s t p t4 et) = _
---   showSC (QuantifE s t q l_c t5 et) = _
 --   showSC (FldAccE s t et f) = _
 --   showSC (TupleE s t l_et) = _
 --   showSC (CastE s t t3 et) = _
---   showSC (ListE s t l l_et) = _
---   showSC (NotDeriv s t b v et) = _
+instance SCasp UnaOp where
+  showSingle (UArith u) = mempty
+  showSingle (UBool UBneg) = pretty "not"
 
 instance Arg (Var, Tp) where
   mkAtom (var, tp) = mkAtom tp <> pretty "_" <> mkAtom var
@@ -215,8 +179,8 @@ instance Arg Tp where
     _ -> "UnsupportedType"
 
 instance SCasp Val where
-  showSC (BoolV b) = rule $  pretty "Bool"
-  showSC (IntV i) = rule $  pretty "Int"
-  showSC (StringV l_c) = rule $  pretty l_c
-  showSC (RecordV c l_p_fv) = rule $  pretty "unsupported, sorry"
-  showSC ErrV = rule $  pretty "Error"
+  showSingle (BoolV b) = pretty "Bool"
+  showSingle (IntV i) = pretty "Int"
+  showSingle (StringV l_c) = pretty l_c
+  showSingle (RecordV c l_p_fv) = pretty "unsupported, sorry"
+  showSingle ErrV = pretty "Error"
