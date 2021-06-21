57c3742e531a2107d517885c81152b97bb648321
Meowyam <maryam@chilon.net>
2021-06-21 15:35:54 +0800

    update Answers to include more generalised aggregates

--- src/ToGF/FromL4/ToAnswers.hs
+++ src/ToGF/FromL4/ToAnswers.hs
@@ -69,7 +69,7 @@ class Answerable x where
     toAnswers :: x -> [GStatement]
 
 instance Answerable (VarDecl t) where
-  toAnswers v = [GApp1] <*>  [toPred v]
+  toAnswers v = [(`GApp` dummySubj)] <*> [toPred v]
 
 instance Answerable (Program a) where
   toAnswers = concatMap toAnswers . filter isPred.globalsOfProgram
@@ -86,6 +86,9 @@ isPred' :: Tp -> Bool
 isPred' (FunT _ BoolT) = True
 isPred' (FunT _ t2) = isPred' t2
 isPred' _ = False
+
+dummySubj :: GArg
+dummySubj = GAVar (GV(GString "dummy"))
 
 -- -- patterns
 
