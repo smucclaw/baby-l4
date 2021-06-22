11b0bff5ca3bb743d20053f2d916c432b97bd06b
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-06-22 20:57:13 +0800

    Add support for arity in encodings & child types

--- src/ToDA2.hs
+++ src/ToDA2.hs
@@ -19,106 +19,159 @@ createDSyaml :: Program Tp -> IO ()
 createDSyaml p = putDoc $ showDS p
 
 
--- Perhaps instead of using Maybe, an Either would be better. 
+-- Perhaps instead of using Maybe, an Either would be better.
 -- With Left :: Doc ann = PP.emptyDoc, and right :: WithProgram (Doc ann) = concretized
-data DSBlock t = DSBlock { blkName  :: String
-                         , blkType  :: Either String Tp 
-                         , blkCard  :: Maybe Int -- cardinality
-                         , blkEncodings :: String
-                         , blkAttrs :: [Maybe (DSBlock t)]
-                         , blkUI    :: [Maybe t] -- ask, tell, any, another
-                         , blkSource :: Maybe String
+data DSBlock t = DSBlock { blkName  :: String               -- block name
+                         , blkType  :: String               --
+                         , blkCard  :: Maybe Int            -- cardinality, everything is assumed to be a list
+                         , blkEncodings :: String           -- associated sCASP encoding
+                         , blkAttrs :: [Maybe (DSBlock t)]  --
+                         , blkUI    :: [Maybe t]            -- ask, tell, any, another
+                         , blkSource :: Maybe String        -- only appears if block is a child attribute, with an "Object type"
                          }
 
 
 instance (Show t) => DSYaml (DSBlock t) where
   showDS DSBlock { blkName, blkType, blkCard, blkEncodings, blkAttrs, blkUI, blkSource} =
     hang 2 $ "-" <+> vsep [ "name:" <+> pretty blkName
-                          , "type:" <+> either pretty showDS blkType 
-                          , "minimum: 0 # Change cardinality accordingly" --cardinality goes here
-                          -- , "ask/tell/any/other:" -- ui goes here
+                          , "type:" <+> pretty blkType
+                          , "minimum: 0" --cardinality goes here
+                          -- TODO: "ask/tell/any/other:"
                           , "encodings:"  , indent 2 $ "-" <+> pretty blkEncodings -- TODO : add functionality for list-encoding
                           , putSource blkSource <> putAttrs blkAttrs <>  PP.line
                           ]
       where putAttrs ba = case ba of
-              [Nothing] -> PP.emptyDoc 
+              [Nothing] -> PP.emptyDoc
               _  -> "attributes:" <> PP.line <> indent 2 (showDSlist $ catMaybes ba) -- attributes here (one-level only)
             putSource (Just x) = "source:" <+> pretty x
             putSource Nothing = PP.emptyDoc
-              
 
--- ClassDecl { 
---     annotOfClassDecl = _           
---   , nameOfClassDecl = ClsNm "Sign"
---   , defOfClassDecl = ClassDef { supersOfClassDef = [ ClsNm "Sign"
---                                                    , ClsNm "Class"
---                                                    , ClsNm "Object" ]
---                               , fieldsOfClassDef = [ FieldDecl { annotOfFieldDecl = _
---                                                                , nameOfFieldDecl = FldNm "beat"
---                                                                , tpOfFieldDecl = FunT
---                                                                    ( ClassT ( ClsNm "Sign" ) ) BoolT }]} }
---
--- ClassDecl { annotOfClassDecl = _
---           , nameOfClassDecl = ClsNm "Player"
---           , defOfClassDecl = ClassDef { supersOfClassDef = [ ClsNm "Player"
---                                                            , ClsNm "Class"
---                                                            , ClsNm "Object" ]
---                                       , fieldsOfClassDef = [ FieldDecl { annotOfFieldDecl = _ 
---                                                                        , nameOfFieldDecl = FldNm "throw"
---                                                                        , tpOfFieldDecl = FunT ( ClassT ( ClsNm "Sign" ) ) BoolT } ] } }
 
--- ClassDecl { annotOfClassDecl = _
---           , nameOfClassDecl = ClsNm "Game"
---           , defOfClassDecl = ClassDef { supersOfClassDef = [ ClsNm "Game"
---                                                            , ClsNm "Class"
---                                                            , ClsNm "Object" ]
---                                       , fieldsOfClassDef = [ FieldDecl { annotOfFieldDecl = _
---                                                                        , nameOfFieldDecl = FldNm "participate_in"
---                                                                        , tpOfFieldDecl = FunT ( ClassT ( ClsNm "Player" ) ) BoolT
---                                                                        }
---                                                            , FieldDecl { annotOfFieldDecl = _
---                                                                        , nameOfFieldDecl = FldNm "win"
---                                                                        , tpOfFieldDecl = FunT ( ClassT ( ClsNm "Player" ) ) BoolT
---                                                                        }
---                                                            ] } } ]
+--  nameOfClassDecl = ClsNm { stringOfClassName = "Service" }
+--             , defOfClassDecl = ClassDef
+--                 { supersOfClassDef =
+--                     [ ClsNm { stringOfClassName = "Service" }
+--                     , ClsNm { stringOfClassName = "Class" }
+--                     , ClsNm { stringOfClassName = "Object" }
+--                     ]
+--                 , fieldsOfClassDef =
+--                     [ FieldDecl
+--                         { annotOfFieldDecl = LocTypeAnnot
+--                             { locAnnot =
+--                             , typeAnnot = OkT
+--                             }
+--                         , nameOfFieldDecl = FldNm { stringOfFieldName = "isLegalService" }
+--                         , tpOfFieldDecl = ClassT
+--                             ( ClsNm { stringOfClassName = "Boolean" } )
+--                         }
+--                     , FieldDecl
+--                         { annotOfFieldDecl = LocTypeAnnot
+--                             { locAnnot =
+--                             , typeAnnot = OkT
+--                             }
+--                         , nameOfFieldDecl = FldNm { stringOfFieldName = "isLawRelatedService" }
+--                         , tpOfFieldDecl = ClassT
+--                             ( ClsNm { stringOfClassName = "Boolean" } )
+--                         }
+--                     , FieldDecl
+--                         { annotOfFieldDecl = LocTypeAnnot
+--                             { locAnnot =
+--                             , typeAnnot = OkT
+--                             }
+--                         , nameOfFieldDecl = FldNm { stringOfFieldName = "isListedInFourthSchedule" }
+--                         , tpOfFieldDecl = ClassT
+--                             ( ClsNm { stringOfClassName = "Boolean" } )
+--                         }
+--                     ]
+--                 }
+--             }
 
+-- nameOfClassDecl = ClsNm { stringOfClassName = "LegalPractitioner" }
+--             , defOfClassDecl = ClassDef
+--                 { supersOfClassDef =
+--                     [ ClsNm { stringOfClassName = "LegalPractitioner" }
+--                     , ClsNm { stringOfClassName = "Person" } -- TODO: source for class decls that transpile to objects
+--                     , ClsNm { stringOfClassName = "Class" }
+--                     , ClsNm { stringOfClassName = "Object" }
+--                     ]
+--                 , fieldsOfClassDef =
+--                     [ FieldDecl
+--                         { annotOfFieldDecl = LocTypeAnnot
+--                             { locAnnot =
+--                             , typeAnnot = OkT
+--                             }
+--                         , nameOfFieldDecl = FldNm { stringOfFieldName = "Locum_Solicitor" }
+--                         , tpOfFieldDecl = ClassT
+--                             ( ClsNm { stringOfClassName = "Boolean" } )
+--                         }
+--                     , FieldDecl
+--                         { annotOfFieldDecl = LocTypeAnnot
+--                             { locAnnot =
+--                             , typeAnnot = OkT
+--                             }
+--                         , nameOfFieldDecl = FldNm { stringOfFieldName = "Primary_Occupation" }
+--                         , tpOfFieldDecl = ClassT
+--                             ( ClsNm { stringOfClassName = "Boolean" } )
+--                         }
+--                     ]
+--                 }
+--             }
 
 
 classDeclToBlock :: ClassDecl ct -> DSBlock ct
 classDeclToBlock ClassDecl { nameOfClassDecl, defOfClassDecl } =
   DSBlock { blkName = lowercase clnm
-          , blkType = Left "String" -- This needs to show "boolean" for types that are supported
+          , blkType = "String" -- This needs to show "boolean" for types that are supported
           , blkCard = Nothing
-          , blkEncodings = lowercase clnm ++ "(X)"
-          , blkAttrs = mapAttrs $ fieldsOfClassDef defOfClassDecl 
+          , blkEncodings = lowercase clnm ++ "(X)" -- TODO: Arity of parent encodings
+          , blkAttrs = mapAttrs $ fieldsOfClassDef defOfClassDecl
           , blkUI = undefined
           , blkSource = Nothing
           }
   where clnm = (\(ClsNm name) -> name) nameOfClassDecl
-        mapAttrs x = case x of 
+        mapAttrs x = case x of
           [] -> [Nothing]
           _  -> map (Just . fieldDeclToBlock) x
 
 fieldDeclToBlock :: FieldDecl ct -> DSBlock ct
 fieldDeclToBlock (FieldDecl _ (FldNm fldnm) fieldtype) =
-  DSBlock { blkName = lowercase fldnm
-          , blkType = eitherTp "Object" fieldtype fieldtype -- This needs to show the "boolean" for types that are supported
+  let blockArity = getArity fieldtype
+      outname = lowercase fldnm
+  in
+  DSBlock { blkName = outname
+          , blkType = getBlocktype fieldtype
           , blkCard = Nothing
-          , blkEncodings = (lowercase fldnm ++) $ either id id $ eitherTp "(X,Y)" "(Y)" fieldtype
+          , blkEncodings = showEncoding outname blockArity
           , blkAttrs = [Nothing]
           , blkUI = undefined
           , blkSource = showFTname fieldtype  -- for supported types, there is no source block
           }
 
+
 eitherTp :: String -> a -> Tp -> Either String a
 eitherTp x1 x2 tp = if elem tp [BoolT, IntT] then Right x2 else Left x1
 
-showFTname :: Tp -> Maybe String 
-showFTname tp = case tp of 
-  (FunT x BoolT)        -> showFTname x
+getBlocktype :: Tp -> String
+getBlocktype tp = case tp of
+  (FunT x y)  -> "Object"
+  (ClassT (ClsNm "Boolean")) -> "Boolean"
+
+showFTname :: Tp -> Maybe String
+showFTname tp = case tp of
+  (FunT x y)        -> showFTname x
+  (ClassT (ClsNm "Boolean")) -> Nothing
   (ClassT (ClsNm name)) -> Just $ lowercase name
   _                     -> Nothing
 
+getArity :: Tp -> Int
+getArity (FunT x y) = 1 + getArity y
+getArity (ClassT x) = 1
+
+showEncoding :: String -> Int -> String
+showEncoding x 1 = x <> "(X)"
+showEncoding x 2 = x <> "(X,Y)"
+
+lowercase :: String -> String
 lowercase = map toLower
 
 -- type WithProgram = Reader (Program Tp)
@@ -137,10 +190,3 @@ instance DSYaml (Program Tp) where
          , "terms: "
          , "options: " <> PP.line
          ]
-
-
-instance DSYaml Tp where
-  showDS tp = case tp of
-    BoolT         -> "Boolean"
-    IntT          -> "Number"
-    _             -> "Unsupported Type"
