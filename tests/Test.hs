f67a93624645ad61f5079b15451dba997d6ce0cc
Inari Listenmaa <inari.listenmaa@gmail.com>
2021-09-01 15:43:12 +0800

    Tests for checkNFlip

--- tests/Test.hs
+++ tests/Test.hs
@@ -14,6 +14,7 @@ main = do
     [ disambiguateTests pgf -- from DisambiguateTest
     , testGetAtoms -- from ToGFTest
     , testDescription -- from ToGFTest
+    , testFlipDescription -- from ToGFTest
     ]
 
 
--- tests/ToGFTest.hs
+++ tests/ToGFTest.hs
@@ -1,7 +1,14 @@
 module ToGFTest where
 
 import Syntax
+    ( ClassName(ClsNm, stringOfClassName),
+      Mapping(..),
+      VarDecl(..),
+      Tp(IntT, OkT, FunT, ClassT),
+      Description(Descr),
+      parseDescription )
 import Annotation
+import ToGF.NormalizeSyntax
 import ToGF.FromL4.ToAnswers ( getAtoms )
 import ToGF.GenerateLexicon ( AtomWithArity(AA) )
 import Test.Tasty ( testGroup, TestTree )
@@ -50,3 +57,39 @@ testDescription = testGroup  "Test the parseDescription"
     testCase "parse with no arguments" $
         parseDescription  "I am super high" @?= Descr "I am super high" []
     ]
+
+testFlipDescription :: TestTree
+testFlipDescription = testGroup "test checkNFlip"
+   [testCase "flip binary pred that has wrong order" $
+        checkNFlip lexicon1 participate_flipped  @?= participate_default
+    ,
+    testCase "don't flip binary pred that has right order" $
+        checkNFlip lexicon1 participate_default @?= participate_default
+    ,
+    testCase "don't change unary predicate" $
+        checkNFlip lexicon1 unary_predicate_shouldnt_flip @?= unary_predicate_shouldnt_flip
+    ,
+    testCase "don't flip with wrong lexicon" $
+        checkNFlip lexicon2 participate_flipped @?= participate_flipped
+    ,
+    testCase "don't flip when names don't match" $
+        checkNFlip lexicon2 participate_flipped_wrong_name @?= participate_flipped_wrong_name
+   ]
+   where
+       lexicon1 :: Mapping ()
+       lexicon1 = Mapping () "participate" (Descr "participates in" ["Player", "Game"])
+
+       lexicon2 :: Mapping ()
+       lexicon2 = Mapping () "participate" (Descr "participates in" ["Foo", "Bar"])
+
+       participate_default :: VarDecl ()
+       participate_default = Fun2 () "participate" "Player" "Game"
+
+       participate_flipped  :: VarDecl ()
+       participate_flipped = Fun2 () "participate" "Game" "Player"
+
+       participate_flipped_wrong_name  :: VarDecl ()
+       participate_flipped_wrong_name = Fun2 () "FOO" "Game" "Player"
+
+       unary_predicate_shouldnt_flip :: VarDecl ()
+       unary_predicate_shouldnt_flip = VarDecl () [] (Arg1 "Player")
