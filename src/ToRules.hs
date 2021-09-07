96cd6cbcb6fe9950c70e479e2158720ab74a3129
Alfred A <20140832+alfredats@users.noreply.github.com>
2021-09-07 20:20:58 +0800

    Preconditions for rules are working now

--- src/ToRules.hs
+++ src/ToRules.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module ToRules where
 
-import Prettyprinter 
+import Prettyprinter
 import Prettyprinter.Render.Text (putDoc)
 import Syntax
 import L4LSP (arNameToString)
@@ -95,18 +95,18 @@ data ConditionalElement
     deriving Show
 
 instance ShowClara ConditionalElement where
-    showClara (ConditionalElement tn args) = 
-        brackets (pretty (capitalize tn) <+> parens (hsep $ punctuate comma (map showClara args)))
+    showClara (ConditionalElement tn args) =
+        brackets (pretty (capitalize tn) <+> (hsep (map (parens . showClara) args)))
     showClara (ConditionalEval expr) = pretty "ConditionalEval not implemented yet"
 
 -- TODO: Figure out what the replacement for String in CEEquality should be
 --          Potential alternatives: OkT? ClsNm?
 --       *To ask Martin*
-data CEArg = CEBinding ProdVarName ProdFieldName | CEEquality ProdFieldName String deriving Show 
+data CEArg = CEBinding ProdVarName ProdFieldName | CEEquality ProdFieldName String deriving Show
 
 instance ShowClara CEArg where
     showClara (CEEquality fn fv) = pretty "=" <+> pretty fn <+> pretty fv
-    showClara (CEBinding vn fn) = pretty "Bindings not implemented yet"
+    showClara (CEBinding vn fn) = pretty "=" <+> pretty "?" <> pretty vn <+> pretty fn
 
 -- We restrict the format of the rules to a singular post condition (to support prolog-style syntax within l4)
 data RuleAction = InsertLogical Typename [Argument] deriving Show
@@ -131,20 +131,36 @@ varDeclToProdVarName = undefined
 capitalize :: String -> String
 capitalize xs = toUpper (head xs) : tail xs
 
+-- remove this after merging to main (add "import SyntaxManipulations (appToFunArgs")
+appToFunArgs :: [Expr t] -> Expr t -> (Expr t, [Expr t])
+appToFunArgs acc (AppE _ f a) = appToFunArgs (a:acc) f
+appToFunArgs acc t = (t, acc)
+
 precondToRuleCondition :: Expr t -> RuleCondition
-precondToRuleCondition fApp@(AppE _ _ _) =
-    RuleCondition $ exprToConditionalElement fApp
-precondToRuleCondition (BinOpE _ binOp arg1 arg2) 
+precondToRuleCondition fApp@AppE {} =
+    let (fname, args) = appToFunArgs [] fApp
+    in RuleCondition $ exprToConditionalElement fname args
+precondToRuleCondition (BinOpE _ binOp arg1 arg2)
     | binOp == BBool BBand = And (precondToRuleCondition arg1) (precondToRuleCondition arg2)
     | binOp == BBool BBor = Or (precondToRuleCondition arg1) (precondToRuleCondition arg2)
--- precondToRuleCondition (UnaOpE _ unaOp arg)
---     | unaOp == 
-    
+precondToRuleCondition (UnaOpE _ unaOp arg)
+    | unaOp == UBool UBnot = Not (precondToRuleCondition arg)
+precondToRuleCondition _ = error "beep boop"
+
 getName :: Expr t -> VarName
 getName = nameOfQVarName . nameOfVar . varOfExprVarE
 
-exprToConditionalElement :: Expr t -> ConditionalElement 
-exprToConditionalElement (AppE _ func arg ) = ConditionalElement (getName func) [CEEquality (getName func) (getName arg)]
+exprToConditionalElement :: Expr t -> [Expr t] -> ConditionalElement
+exprToConditionalElement func@(VarE _ _) args = ConditionalElement (getName func) (map exprToCEArg $ zip [0.. ((length args)-1)] args)
+exprToConditionalElement _ _ = error "tbd"
+
+defArg :: Int -> ProdFieldName
+defArg x = "arg" ++ show x
+
+exprToCEArg :: (Int, Expr t) -> CEArg -- assumption: either local or global var expr
+exprToCEArg (num, VarE _ (LocalVar name _)) = CEBinding (nameOfQVarName name) (defArg num) 
+exprToCEArg (num, VarE _ (GlobalVar name)) = CEEquality (defArg num) (nameOfQVarName name)
+exprToCEArg _ = error "nope"
 
 postcondToRuleAction :: Expr t -> RuleCondition
 postcondToRuleAction = undefined
@@ -153,6 +169,7 @@ postcondToRuleAction = undefined
 astToRules :: Program (Tp ()) -> IO ()
 astToRules x = do
     let lrRules = map filterRule $ rulesOfProgram x
+        gdRules = rights lrRules
     print $ lefts lrRules
-    print $ head $ rights lrRules
-    putDoc $ showClara $ head $ rights lrRules
+    print $ head $ gdRules
+    putDoc $ showClara (gdRules !! 5)
