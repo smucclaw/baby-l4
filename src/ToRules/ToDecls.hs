499c203dbb4c9cd241d0e59fc4a2ed92ab484b97
Alfred A <20140832+alfredats@users.noreply.github.com>
2021-10-21 11:10:27 +0800

    Add object declarations

---
+++ src/ToRules/ToDecls.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE NamedFieldPuns #-}
+module ToRules.ToDecls where
+
+import Prettyprinter
+import Syntax
+import SyntaxManipulation
+import Annotations
+import Data.Either (rights)
+import ToRules.Types
+
+
+data DeclError = DeclError String deriving Show
+
+filterDecls :: (Show t) => VarDecl t -> Either DeclError (VarDecl t, [Tp t])
+filterDecls dl@(VarDecl _ nm x) = case spine [] x of
+    (tps, ClassT _ (ClsNm "Boolean")) -> Right (dl, tps)
+    (_, _) -> Left $ DeclError $ "Not transpiled to class: " ++ show nm
+
+varDeclToProductionClassDecl :: VarDecl t -> [Tp t] -> ProductionClassDecl
+varDeclToProductionClassDecl (VarDecl _ nm _) tp = ProductionClassDecl nm fs
+    where fs = map tpToProductionClassField $ zip [0..(length tp- 1)] tp
+
+tpToProductionClassField :: (Int, Tp t) -> ProductionClassField
+tpToProductionClassField (pos, c) = ProductionClassField ("arg" ++ show pos) (yieldNativeTp . stringOfClassName . classNameOfTp $ c)
+    where yieldNativeTp x = if x `elem` ["Integer", "Boolean", "Float"] then x else "String"
+
+astToDecls :: Program (Tp ()) -> IO ()
+astToDecls x = do
+    let decls = map filterDecls $ globalsOfProgram x
+        gd = rights decls
+        trans = map (uncurry varDeclToProductionClassDecl) gd
+    mapM_ (print . (<>) line . showDrools) trans
