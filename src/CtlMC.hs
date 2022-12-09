a0cbf70d856d1abcca2ef50b6f741734311c0be0
Martin Strecker <martin.strecker@irit.fr>
2022-12-09 18:15:45 +0100

    improved fixed point operator

--- src/CtlMC.hs
+++ src/CtlMC.hs
@@ -5,7 +5,7 @@
 module CtlMC where
 
 import L4.Syntax
-import L4.SyntaxManipulation (conjExpr, disjExpr, implExpr, abstractQ, abstractF, liftVarBy, conjsExpr, disjsExpr, applyVars, mkVarE, mkEq, mkFloatConst, mkFunTp, index, indexListFromTo, gteExpr, eqExpr, mkIntConst, liftVar, funArgsToApp, notExpr, liftType)
+-- import L4.SyntaxManipulation (conjExpr, disjExpr, implExpr, abstractQ, abstractF, liftVarBy, conjsExpr, disjsExpr, applyVars, mkVarE, mkEq, mkFloatConst, mkFunTp, index, indexListFromTo, gteExpr, eqExpr, mkIntConst, liftVar, funArgsToApp, notExpr, liftType)
 
 import L4.PrintProg (renameAndPrintExpr, renameExpr, printExpr, printARName )
 import Data.List (find)
@@ -29,8 +29,16 @@ fp f s =
 postStates :: TA t -> Loc -> [Loc]
 postStates aut s = [targetOfTransition tr | tr <- transitionsOfTA aut, s == sourceOfTransition tr ]
 
-fpOpUTEG :: TA (Tp()) -> S.Set Loc -> S.Set Loc
-fpOpUTEG aut t = S.filter (\s -> not (S.null (S.intersection (S.fromList (postStates aut s)) t))) t
+postStatesSet :: TA t -> S.Set Loc -> S.Set Loc
+--postStatesSet aut ss = S.unions (S.map (S.fromList . (postStates aut)) ss)
+postStatesSet aut = S.unions . (S.map (S.fromList . (postStates aut)))
+
+fpOpUTEG_old :: TA (Tp()) -> S.Set Loc -> S.Set Loc
+fpOpUTEG_old aut t = S.filter (\s -> not (S.null (S.intersection (S.fromList (postStates aut s)) t))) t
+
+fpOpUTEG :: TA (Tp()) -> S.Set Loc -> S.Set Loc -> S.Set Loc
+fpOpUTEG aut sf = (S.intersection sf) . (postStatesSet aut)
+
 
 ctlCheck :: TA (Tp()) -> Expr (Tp()) -> S.Set Loc
 ctlCheck  aut (ValE _ (BoolV True)) = S.fromList (locsOfTA aut)
@@ -39,18 +47,26 @@ ctlCheck  aut ve@VarE{} = S.fromList [lc | lc <- locsOfTA aut, (lc, ve)  `elem` 
 ctlCheck aut (UnaOpE _ (UBool UBnot) e) = S.difference (S.fromList (locsOfTA aut)) (ctlCheck aut e)
 ctlCheck aut (BinOpE _ (BBool BBand) e1 e2) = S.intersection (ctlCheck aut e1) (ctlCheck aut e2)
 ctlCheck aut (BinOpE _ (BBool BBor) e1 e2) = S.union (ctlCheck aut e1) (ctlCheck aut e2)
-ctlCheck aut (UnaOpE _ (UTemporal UTEG) e) = fp (fpOpUTEG aut) (ctlCheck aut e)
+--ctlCheck aut (UnaOpE _ (UTemporal UTEG) e) = fp (fpOpUTEG_old aut) (ctlCheck aut e)
+ctlCheck aut (UnaOpE _ (UTemporal UTEG) e) = let sf = (ctlCheck aut e) in fp (fpOpUTEG aut sf) sf
 ctlCheck _aut _ = S.empty
 
+
+setOkT :: Functor f => f (Tp()) -> f (Tp())
+setOkT = fmap (\_t -> OkT)
 
 proveAssertionCTL :: Program (Tp ()) -> ValueKVM -> Assertion (Tp ()) -> IO ()
 proveAssertionCTL prg _instr asrt =
   let ta = head (automataOfProgram prg) in
+  let expr_w_types = (exprOfAssertion asrt) in
+  let expr_wo_types = setOkT expr_w_types in
     do
         putStrLn ("Launching CTL checker on " ++ printARName (nameOfAssertion asrt))
-        putStrLn (show (exprOfAssertion asrt))
+        putStrLn (show expr_w_types)
+        putStrLn (show expr_wo_types)
         putStrLn (show (labellingOfTA ta))
-        putStrLn (show (ctlCheck ta (exprOfAssertion asrt)))
-        if S.member (initialLocOfTA ta) (ctlCheck ta (exprOfAssertion asrt))
+        putStrLn (show (ctlCheck ta expr_wo_types))
+        -- putStrLn (show  (fpOpUTEG ta (S.fromList [Loc {nameOfLoc = "loc0"},Loc {nameOfLoc = "loc1"}])))
+        if S.member (initialLocOfTA ta) (ctlCheck ta expr_wo_types)
         then putStrLn "Property satisfied"
         else putStrLn "Property not satisfied"
